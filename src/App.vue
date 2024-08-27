8d8f2f21b5e054ec2989770e46dff0e742ee55c7
lorenzo paciotti <lupacche@gmail.com>
2024-08-27 10:27:21 +0200

    card component and props

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 
+import {ref,watch} from "vue"
+
   import q from "./data/quizes.json"
 
-  import {ref,watch} from "vue"
+    import Card from "./components/Card.vue"
 
   const quizes = ref(q)
   const search = ref("")
@@ -21,13 +23,7 @@
       <input v-model.trim="search" type="text" placeholder="cerca..."></input>  
     </header>
     <div class="options-container">
-      <div v-for="quiz in quizes" :key="quiz.id" class="card">
-        <img :src="quiz.img" alt="">
-        <div class="div card-text">
-          <h2>{{quiz.name}}</h2>
-          <p>{{ quiz.questions.length }} conta</p>
-        </div>
-      </div>
+      <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz"/>
     </div>
   </div>
 </template>
@@ -41,24 +37,6 @@
     align-items: center;
   }
 
-  .card{
-    width: 310px;
-    overflow: hidden;
-    border-radius: 2%;
-    box-shadow:1px;
-    margin-bottom: 35px;
-    margin-right:20px;
-    cursor:pointer;
-  }
 
-  .card img{
-    width:100%;
-    height:190px;
-    margin:0;
-  }
-
-  .card .card-text{
-    font-weight: bold;
-  }
 
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,43 @@
+<script setup>
+
+import {defineProps} from "vue"
+
+const {quiz} = defineProps(["quiz"])
+//equiv to const quiz = props.quiz
+
+</script>
+
+
+<template>
+    <div class="card">
+        <img :src="quiz.img"></img>
+        <div class="card-text">
+            <h2>{{ quiz.name }}</h2>
+            <p>{{ quiz.questions.length }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+.card{
+    width: 310px;
+    overflow: hidden;
+    border-radius: 2%;
+    box-shadow:1px;
+    margin-bottom: 35px;
+    margin-right:20px;
+    cursor:pointer;
+  }
+
+  .card img{
+    width:100%;
+    height:190px;
+    margin:0;
+  }
+
+  .card .card-text{
+    font-weight: bold;
+  }
+
+</style>
